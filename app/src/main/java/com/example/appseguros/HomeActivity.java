df70f5f31ad8761cab66355715a01308cfe2207b
Miguel Monje <mmv776@alumnos.unican.es>
2023-09-29 10:13:29 +0200

    Added resources

--- app/src/main/java/com/example/appseguros/HomeActivity.java
+++ app/src/main/java/com/example/appseguros/HomeActivity.java
@@ -1,15 +1,24 @@
 package com.example.appseguros;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
+    androidx.constraintlayout.widget.ConstraintLayout altaBricolajeButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        altaBricolajeButton = findViewById(R.id.gestionServicio);
+        altaBricolajeButton.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this,AltaBricolajeActivity.class);
+            startActivity(intent);
+        });
 
 
     }
