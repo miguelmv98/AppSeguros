a714f0c5c0844025b7018795ead468b969852884
Carmina López <74677482+carminalp@users.noreply.github.com>
2023-10-01 21:31:48 +0200

    Final alta_bricolaje

---
+++ app/src/main/java/com/example/appseguros/AltaBricolajeActivity.java
@@ -0,0 +1,37 @@
+package com.example.appseguros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+public class AltaBricolajeActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alta_bricolaje);
+
+        String[] type = new String[] {"Bed-sitter", "Single", "1- Bedroom"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                R.layout.dropdown_item,
+                type
+        );
+
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.filled_exposed);
+        autoCompleteTextView.setAdapter(adapter);
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(AltaBricolajeActivity.this, autoCompleteTextView.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/appseguros/RegistroActivity.java
+++ app/src/main/java/com/example/appseguros/RegistroActivity.java
@@ -2,13 +2,20 @@ package com.example.appseguros;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RegistroActivity extends AppCompatActivity {
-
+    Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registro);
+        registerButton = findViewById(R.id.Registrarse);
+        registerButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this,AltaBricolajeActivity.class);
+            startActivity(intent);
+        });
     }
 }
