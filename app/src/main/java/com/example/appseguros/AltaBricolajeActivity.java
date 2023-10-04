24af88c9c618c922fa6b302ab435e1c8e872c01b
Miguel Monje Velarde <mmv776@alumnos.unican.es>
2023-10-05 00:18:07 +0200

    Corregidos conflictos en main

--- app/src/main/java/com/example/appseguros/AltaBricolajeActivity.java
+++ app/src/main/java/com/example/appseguros/AltaBricolajeActivity.java
@@ -1,18 +1,37 @@
 package com.example.appseguros;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AltaBricolajeActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+public class AltaBricolajeActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alta_bricolaje);
 
+        String[] type = new String[] {"Electricidad", "Carpintería", "Fontanería"};
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                R.layout.dropdown_item,
+                type
+        );
+
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.filled_exposed);
+        autoCompleteTextView.setAdapter(adapter);
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(AltaBricolajeActivity.this, autoCompleteTextView.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
