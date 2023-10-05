a4ac8ab12bbc5ad4a89a1fe00232c3ce201962fa
Carmina López <74677482+carminalp@users.noreply.github.com>
2023-10-05 02:36:11 +0200

    Creación de layout historial bricolaje

--- app/src/main/java/com/example/appseguros/HistorialBricolajeActivity.java
+++ app/src/main/java/com/example/appseguros/HistorialBricolajeActivity.java
@@ -2,13 +2,21 @@ package com.example.appseguros;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class HistorialBricolajeActivity extends AppCompatActivity {
 
+    androidx.constraintlayout.widget.ConstraintLayout homeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historial_bricolaje);
+
+        homeButton = findViewById(R.id.homeButtonSiniestro);
+        homeButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this,HomeActivity.class);
+            startActivity(intent);
+        });
     }
 }
